02bccce7d1e647ec6ca881e3a7607359f84006f1
rvmoura <rvmoura.96@gmail.com>
2019-01-04 18:42:50 -0200

    Fixing some typos on tests.

--- core/tests/tests_models.py
+++ core/tests/tests_models.py
@@ -19,8 +19,8 @@ class PontosTuristicosTest(TestCase):
         """
         O atributo Ponto Turistico deve ser uma instancia de PontosTuristicos.
 
-        O Objeto self.pontos_turisticos deverá sera uma instancia do
-        Modelo PontosTuristicos.
+        O atributo pontos_turisticos deverá sera uma instancia do
+        modelo PontosTuristicos.
         """
         self.assertTrue(isinstance(self.ponto_turistico, PontosTuristicos))
 
@@ -28,7 +28,7 @@ class PontosTuristicosTest(TestCase):
         """
         O retorno do método __str__ deve ser o nome da instancia.
 
-        O valor retornado no método __str deverá ser o valor do atributo
+        O valor retornado no método __str__ deverá ser o valor do atributo
         nome.
         """
         self.assertEqual(self.ponto_turistico.__str__(), 'Bar do Zé')
