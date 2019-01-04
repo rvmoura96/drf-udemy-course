ceeaca1d5672635857873d5672723e4d8b652d03
rvmoura <rvmoura.96@gmail.com>
2019-01-04 16:52:14 -0200

    Criação da App Core e modelagem da mesma.

---
+++ core/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+from .models import PontosTuristicos
+# Register your models here.
+
+admin.site.register(PontosTuristicos)
---
+++ core/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+
+class PontosTuristicos(models.Model):
+    name = models.CharField(max_length=150)
+    descricao = models.TextField()
+    aprovado = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.nome
